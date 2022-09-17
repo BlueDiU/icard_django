7c923ae505d8ed18b53fe195ede63e26de98e3c4
Josue Rivas <reycoc2311@gmail.com>
2022-09-16 18:53:46 -0600

    jwt timelife

--- icard/settings.py
+++ icard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # USERS 
 AUTH_USER_MODEL = 'users.User'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+SIMPLE_JWT = {
+ 'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=120)
+}
