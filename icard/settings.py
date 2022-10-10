3f7e988c5fdc7461db54605d1e273995825863bc
josuerivas <reycoc2311@gmail.com>
2022-10-09 18:36:35 -0600

    Fix auth bug

--- icard/settings.py
+++ icard/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'users'
 ]
